0aa0b0338856389df8a14e057410360b0a6c049a
Marin Takanov <mbtakanov@gmail.com>
2025-10-18 22:10:16 +0300

    Add floor, spot, strategy and ticket

--- lld/parking-lot/parking_floor.py
+++ lld/parking-lot/parking_floor.py
@@ -1,13 +1,41 @@
 import threading
-from typing import Dict
+from collections import defaultdict
+from typing import Dict, Optional
 from parking_spot import ParkingSpot
+from vehicle import Vehicle
+from vehicle_size import VehicleSize
 
 
 class ParkingFloor:
     def __init__(self, floor_number: int):
         self.floor_number = floor_number
-        self.splots: Dict[str, ParkingSpot] = {}
+        self.spots: Dict[str, ParkingSpot] = {}
         self._lock = threading.Lock()
 
     def add_spot(self, spot: ParkingSpot):
-        return 1
+        self.spots[spot.get_spot_id()] = spot
+
+    def find_available_spot(self, vehicle: Vehicle) -> Optional[ParkingSpot]:
+        with self._lock:
+            available_spots = [
+                spot
+                for spot in self.spots.values()
+                if spot.is_available() and spot.can_fit_vehicle(vehicle)
+            ]
+
+            if available_spots:
+                available_spots.sort(key=lambda s: s.get_spot_size().value)
+                return available_spots[0]
+
+            return None
+
+    def display_availability(self):
+        print(f"--- Floor #{self.floor_number} ---")
+        available_counts = defaultdict(int)
+
+        for spot in self.spots.values():
+            if spot.is_available():
+                available_counts[spot.get_spot_size()] += 1
+
+        for size in VehicleSize:
+            print(f"{size.value} spots: {available_counts[size]}")
--- lld/parking-lot/parking_spot.py
+++ lld/parking-lot/parking_spot.py
@@ -1,7 +1,45 @@
+import threading
 from vehicle_size import VehicleSize
+from vehicle import Vehicle
 
 
 class ParkingSpot:
     def __init__(self, spot_id: str, spot_size: VehicleSize):
         self.spot_id = spot_id
         self.spot_size = spot_size
+        self.is_occupied = False
+        self.parked_vehicle = None
+        self._lock = threading.Lock()
+
+    def get_spot_id(self) -> str:
+        return self.spot_id
+
+    def get_spot_size(self) -> VehicleSize:
+        return self.spot_size
+
+    def is_available(self) -> bool:
+        with self._lock:
+            return not self.is_occupied
+
+    def park_vehicle(self, vehicle: Vehicle):
+        with self._lock:
+            self.park_vehicle = vehicle
+            self.is_occupied = True
+
+    def unpark_vehicle(self):
+        with self._lock:
+            self.parked_vehicle = None
+            self.is_occupied = False
+
+    def can_fit_vehicle(self, vehicle: Vehicle) -> bool:
+        if self.is_occupied:
+            return False
+
+        if vehicle.get_size() == VehicleSize.SMALL:
+            return self.spot_size == VehicleSize.SMALL
+        elif vehicle.get_size() == VehicleSize.MEDIUM:
+            return self.spot_size in (VehicleSize.MEDIUM, VehicleSize.LARGE)
+        elif vehicle.get_size() == VehicleSize.LARGE:
+            return self.spot_size == VehicleSize.MEDIUM
+        else:
+            return False
